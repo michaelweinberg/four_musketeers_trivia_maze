850f762d47e3c4527f03098d5005e6ac9967f570
jing1988ab <jing1988b@gmail.com>
2022-02-12 23:39:51 -0800

    adding model part

--- TriviaController.py
+++ TriviaController.py
@@ -1,15 +1,15 @@
-from map import Map
+from model import Map, Question
 from TriviaView import TriviaView
 
 
 class TriviaController:
-    def __init__(self, view):
+    def __init__(self):
         self.__map = Map()
         self.__map.generate_map()
         self.__map.generate_player()
         self.__map.print_map()
-        self.__view = view
-        # self.__model = model
+        self.__view = TriviaView()
+        self.__question = Question()
 
     # def print_trivia_maze(self):
     #     return self.__view.print_map(self.__map)
@@ -17,11 +17,10 @@ class TriviaController:
     # def update_map(self):
     #     pass
     #
-    # def get_question_from_db(self):
-    #     question_data = self.__model.get_question_list()
-    #     return self.__view.print_question(question_data)
-    def sample_Q(self):
-        print("QUESTION")
+    def get_question_from_db(self):
+        # question_data = self.__question.get_question()
+        question_data = ["question", "answer"]
+        return question_data
 
     def move_character(self):
         while not self.__map.reach_exit():
@@ -30,26 +29,35 @@ class TriviaController:
             self.__map.generate_player()
             self.__view.print_map(self.__map)
 
+    def answer_question(self):
+        self.__view.print_question(self.get_question_from_db()[0])
+        if input("answer") == self.get_question_from_db()[1]:
+            print("right answer")
+            return True
+        else:
+            print("wrong answer")
+            return False
+
     def enter_room(self, move):
         if move == "a":
             if self.__map.west_room_available() != 2:
                 if self.__map.west_room_available() == 0:
-                    self.sample_Q()
+                    self.answer_question()
                 self.__map.move_west()
         if move == "d":
             if self.__map.east_room_available() != 2:
                 if self.__map.east_room_available() == 0:
-                    self.sample_Q()
+                    self.answer_question()
                 self.__map.move_east()
         if move == "w":
             if self.__map.north_room_available() != 2:
                 if self.__map.north_room_available() == 0:
-                    self.sample_Q()
+                    self.answer_question()
                 self.__map.move_north()
         if move == "s":
             if self.__map.south_room_available() != 2:
                 if self.__map.south_room_available() == 0:
-                    self.sample_Q()
+                    self.answer_question()
                 self.__map.move_south()
 
     def get_room_info(self):
@@ -59,9 +67,9 @@ class TriviaController:
     def get_map(self):
         return self.__map
 
+
 def run():
-    view = TriviaView()
-    newgame = TriviaController(view)
+    newgame = TriviaController()
     newgame.move_character()
 
 
