a324831829d2061a9ad773673ace762795e74df0
Michael Weinberg <michaelweinberg@Michaels-MacBook-Pro.local>
2022-02-14 22:52:35 -0800

    adding trivial controller test file with stubs

---
+++ test/test_trivia_controller.py
@@ -0,0 +1,44 @@
+import unittest
+
+class TriviaControllerTest(unittest.TestCase):
+    def test_init(self):
+        pass
+
+    def test_generate_map(self):
+        pass
+
+    def test_generate_player(self):
+        pass
+
+    def test_movement_available(self, y, x):
+        pass
+
+    def test_block_room(self, y, x):
+        pass
+
+    def test_reach_exit(self):
+        pass
+
+    def test_game_over(self):
+        pass
+
+    def test_get_question_from_db(self):
+        pass
+
+    def test_move_character(self):
+        pass
+
+    def test_answer_question(self):
+        pass
+
+    def test_enter_west(self):
+        pass
+
+    def test_enter_east(self):
+        pass
+
+    def test_enter_north(self):
+        pass
+
+    def test_enter_south(self):
+        pass
